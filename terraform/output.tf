d1f225df0f5a06d6557e45477c73f6fa8e6591f9
Shubham Joshi <40904113+joshi-shubham@users.noreply.github.com>
2025-07-08 14:15:39 +0000

    first commit

--- terraform/output.tf
+++
@@ -1,8 +0,0 @@
-output "dns_ids"{
-    description = "DNS Records to add"
-    value = aws_ses_domain_identity.domail_identity.verification_token
-}
-
-output "lambda_role_arn" {
-  value = aws_iam_role.lambda_role.arn
-}
